0b34edac071a19fd48fd32c2202904a8a6601a40
WIllianMartins2018 <willian.martins01@hotmail.com>
2025-01-09 19:14:41 -0300

    Entidades e Validators
    Criação das Entidades e Validations da aplicação.

---
+++ src/DevIO.Business/Models/Fornecedor.cs
@@ -0,0 +1,14 @@
+﻿using DevIO.Business.Enum;
+using DevIO.Business.ValueObjects;
+
+namespace DevIO.Business.Models;
+
+public class Fornecedor : Entity
+{
+    public string? Nome { get; set; }
+    public string? Documento { get; set; }
+    public ETipoFornecedor TipoFornecedor { get; set; }
+    public Endereco? Endereco { get; set; }
+    public bool Ativo { get; set; }
+}
+
---
+++ src/DevIO.Business/Models/Validations/FornecedorValidation.cs
@@ -0,0 +1,37 @@
+﻿using DevIO.Business.Enum;
+using DevIO.Business.Models.Validations.Documentos;
+using FluentValidation;
+
+namespace DevIO.Business.Models.Validations;
+
+public class FornecedorValidation : AbstractValidator<Fornecedor>
+{
+    public FornecedorValidation()
+    {
+        RuleFor(f => f.Nome)
+            .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+            .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+        When(f => f.TipoFornecedor == ETipoFornecedor.PessoaFisica, () =>
+        {
+            RuleFor(f => f.Documento.Length).Equal(CpfValidacao.TamanhoCpf)
+                .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyName}.");
+
+            RuleFor(f => CpfValidacao.Validar(f.Documento)).Equal(true)
+                .WithMessage("O Documento Fornecido é Inválido");
+
+        });
+
+        When(f => f.TipoFornecedor == ETipoFornecedor.PessoaJuridica, () =>
+        {
+            RuleFor(f => f.Documento.Length).Equal(CnpjValidacao.TamanhoCnpj)
+               .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyName}.");
+
+            RuleFor(f => CnpjValidacao.Validar(f.Documento)).Equal(true)
+               .WithMessage("O Documento Fornecido é Inválido");
+        });
+
+
+
+    }
+}
