22065ad31092b40a2c8ec1d386a2c342b7850ad8
WIllianMartins2018 <willian.martins01@hotmail.com>
2025-01-14 19:46:00 -0300

    Resolvendo Dependencias

---
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,27 @@
+﻿using DevIO.Api.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FornecedoresController : MainController
+    {
+        public FornecedoresController() { }
+
+        [HttpGet]
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos() { }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IEnumerable<FornecedorViewModel>> ObterPorId(Guid id) { }
+
+        [HttpPost]
+        public async Task<ActionResult<FornecedorViewModel>> Adicionar(FornecedorViewModel produtoViewModel) { }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> Atualizar(Guid id, FornecedorViewModel produtoViewModel) { }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> Excluir(Guid id) { }
+    }
+}
---
+++ src/DevIO.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,26 @@
+﻿using DevIO.Api.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProdutosController : MainController
+    {
+        public ProdutosController() { }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos() {  }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IEnumerable<ProdutoViewModel>> ObterPorId(Guid id) { }
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoViewModel>> Adicionar(ProdutoViewModel produtoViewModel) {  }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> Atualizar(Guid id, ProdutoViewModel produtoViewModel) { }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> Excluir(Guid id) { }
+    }
+}
